ede7d4e2e603047adf319d7d4d6f86f0efe1d6d7
Usama <shihabhasib777@gmail.com>
2026-01-15 23:02:14 +0600

    add json data

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa";
 
 const Blog = ({blog}) => {
   
@@ -6,20 +7,34 @@ const Blog = ({blog}) => {
     // const {blog}=props;
 
     return (
-        <div>
+        <div className='m-2'>
             {/* <h1>{blog.id}</h1> */}
             <div className="card bg-base-100 w-96 shadow-sm">
   <figure className="px-10 pt-10">
     <img
       src={blog.coverphoto}
-      alt={blog.title}
+      alt="images"
       className="rounded-xl" />
   </figure>
   <div className="card-body items-center text-center">
+    <div className="author flex items-center text-center justify-around gap-2">
+        <h3>{blog.author}</h3>
+        <div className="avatar">
+  <div className="w-10 rounded-full">
+    <img src={blog.authorImage} />
+  </div>
+</div>
+<FaBookmark  size={25}/>
+    </div>
     <h2 className="card-title">{blog.title}</h2>
     <p>{blog.description}</p>
+    <div className="flex gap-1">
+        {
+    blog.hashtag.map((has)=><p>{has}</p>)
+    }
+    </div>
     <div className="card-actions flex gap-4">
-        <button className='btn btn-primary'>Book Mark</button>
+
       <button className="btn btn-primary">Mark as Read</button>
     </div>
   </div>
